aa9a2d16f0eb8640ad58f89e44eb272babb46dcf
Jason <1046955@students.wits.ac.za>
2017-09-20 20:14:16 +0200

    Renamed the SRS document, changed the db interface class to correctly retrieve data, setup the basic timetable class

--- src/dbInterface.py
+++ src/dbInterface.py
@@ -1,19 +1,23 @@
 import sqlite3 as sql
 
+def main():
+	db = dbInterface("../data/schedex.db")
+	db.getDates()
+
 class dbInterface:
-	def __init__(self):
-		self.__connection = None
-		self.__cursor = None
-	
-	#initialise db connection
-	def connect(self, name):
+	def __init__(self, name):
 		#the next line may be used for testing
 		#name = data\schedex.db
 		self.__connection = sql.connect(name)
-		self.__cursor = connection.cursor()
+		self.__cursor = self.__connection.cursor()
 		
 	def close(self):
 		self.__connection.close()	
 		
 	#create methods for the various commands required	
-	
+	def getDates(self):
+		self.__cursor.execute('SELECT * FROM Staff;')
+		data = self.__cursor.fetchone()
+		print("SQL Version " + str(data[1]))
+		
+main()		
---
+++ src/timetable.py
@@ -0,0 +1,10 @@
+class Timetable:
+	def __init__(self):
+		
+	#receive start and end dates from admin	
+	def getDates(startDate, endDate):
+		#check for weekends using a calendar library or something
+		#and fill an array with all the possible dates for exams
+		#then pull the courses and venues from the db and do graph 
+		#colouring on them
+		
