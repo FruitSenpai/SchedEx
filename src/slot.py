875532a7199b915c265b591eaf0c7f90ff24cd8c
Jason <1046955@students.wits.ac.za>
2017-09-24 10:54:15 +0200

    Added slot class and began work on Google Calendar functionality

---
+++ src/slot.py
@@ -0,0 +1,7 @@
+class Slot:
+	def __init__(self, course, venue, date, time):
+		self.__course = course
+		self.__venue = venue
+		self.__date = date
+		self.__time = time
+	
--- src/timetable.py
+++ src/timetable.py
@@ -1,5 +1,6 @@
 import datetime
 from dbInterface import dbInterface
+from slot import Slot
 
 def main():
 	t = Timetable()
@@ -12,12 +13,11 @@ class Timetable:
 		self.__start = None
 		self.__end = None
 		
-	#receive start and end dates from admin	
+			
 	def getDates(self, start, end):
 		self.__start = start
 		self.__end = end
 		dates = []
-		#end = False
 		
 		while not start == end:
 			if start.weekday() < 5:
@@ -39,6 +39,7 @@ class Timetable:
 		#create array and append each date to it if it's not a 
 		#weekend
 		
+	#receive start and end dates from admin	
 	def createTimetable(self, start, end):
 		dates = self.getDates(start, end)
 		
@@ -55,6 +56,70 @@ class Timetable:
 		#colour nodes with date-venue combos where the venue capacity >= the course size
 		#store course-venue-date combos in timetable table in db
 		#create google calendar events etc
+		
+		#each time node is coloured, instantiate a slot
+		#create an event with an attendees list of all the students who take that course
+		#insert the slot into the timetable table
+		
+		credentials = self.get_credentials()
+		http = credentials.authorize(httplib2.Http())
+		service = discovery.build('calendar', 'v3', http=http)
+		
+		event = {
+		  'summary': 'Google I/O 2017',
+		  'location': '800 Howard St., San Francisco, CA 94103',
+		  'description': 'A chance to hear more about Google\'s developer products.',
+		  'start': {
+			'dateTime': '2017-09-28T09:00:00-07:00',
+	#		'timeZone': 'SouthAfrica',
+		  },
+		  'end': {
+			'dateTime': '2017-09-28T12:00:00-07:00',
+	#		'timeZone': 'SouthAfrica',
+		  },
+		  #can pull list of students for each course from db and add them to attendees list
+		  'attendees': [
+			{'email': 'parryjas@gmail.com'}
+		  ],
+		  'reminders': {
+			'useDefault': False,
+			'overrides': [
+			  {'method': 'email', 'minutes': 24 * 60},
+			  {'method': 'popup', 'minutes': 10},
+			],
+		  },
+		}    
+
+
+		event = service.events().insert(calendarId='primary', body=event).execute()
+		
+def get_credentials(self):
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'calendar-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials		
 		
 main()		
 		
