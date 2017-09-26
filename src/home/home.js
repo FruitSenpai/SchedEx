2aa656b40fd8759eba16c60c837569f99fb306c7
Zaeem Nalla <zaeem.nalla@gmail.com>
2017-09-26 02:22:15 +0200

    Assigned enter key to login button

---
+++ src/home/home.js
@@ -0,0 +1,14 @@
+$(function() {
+
+    // Set enter key to activate the login button
+    $(document).keypress(function(e) {
+        if (e.which === 13){
+            $("#login_button").click();
+        }
+    });
+
+});
+
+function login() {
+    console.log("login");
+}
