d006979dfee7c09cdc601e809f18a85da7931e93
Zaeem Nalla <zaeem.nalla@gmail.com>
2017-09-26 23:13:04 +0200

    Copied base view from generic

---
+++ src/coordinator/coordinator.js
@@ -0,0 +1,17 @@
+$(function() {
+
+    // Short hack used to override tooltip visibility
+    var $search_bar = $("#search_bar");
+    $search_bar.click(function(e) {
+        $("#search_tooltip").hide();
+    });
+    $search_bar.hover(function(e) {
+        $("#search_tooltip").show();
+    });
+
+    // Logout functionality
+    $("#logout").click(function(e) {
+        window.location.replace("../home/home.html");
+    })
+
+});
