92e86803e1dada639b92e622d9e937dac555e41a
Zaeem Nalla <zaeem.nalla@gmail.com>
2017-10-01 19:06:01 +0200

    Added demo data for different user's login credentials

--- src/login/login.js
+++ src/login/login.js
@@ -1,10 +1,23 @@
 $(function() {
 
     // Set enter key to activate the login button
+    var $button = $("#login_button");
     $(document).keypress(function(e) {
         if (e.which === 13){
-            $("#login_button").click();
+            $button.onclick();
         }
     });
 
 });
+
+function login() {
+    var $email = $("#login_email").val();
+    var $password = $("#login_password").val();
+    console.log($email + ", " + $password)
+    if ( ($email === "student@uni.ac.za") && ($password === "student") )
+        window.location.href = "../generic/generic.html";
+    else if ( ($email === "coordinator@uni.ac.za") && ($password === "coordinator") )
+        window.location.href = "../coordinator/coordinator.html";
+    else if ( ($email === "admin@uni.ac.za") && ($password === "admin") )
+        window.location.href = "../admin/admin.html";
+}
